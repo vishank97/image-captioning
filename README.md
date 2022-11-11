fb7de3f54d83356b87ab2fd45baa927e4dc606ee
Vishank Shah <shahvishank947@gmail.com>
2022-11-11 13:59:25 +0530

    formatted streamlit app and edited README

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
-# image-captioning
-Generating Image Captioning model to caption images and deploy on TrueFoundry
+# smart-image-captioning
+
+# Predict caption from images and deploy the ML app on the cloud
+
+## Useful Links
+
+[Taxi Fare Prediction model Deployment Video](https://www.loom.com/share/ecae98fc81754b6ebdb31902e9b4025e)
+
+[Link to Live Demo](https://image-caption-demo-demo-projects.tfy-ctl-euwe1-production.production.truefoundry.com/)
+
+[Colab Notebook](https://drive.google.com/file/d/1WL8cnVmqsWxh9Ok-Ml5axAuxGfnZ1A9S/view?usp=sharing)
+
+Blog with instructions on the run (Coming Soon)
+
+## Description of the problem
+
+The aim is to predict the captions of images using deep learning.
+
+## Model Trained
+
+We used a [pretrained GPT-2 model](https://huggingface.co/nlpconnect/vit-gpt2-image-captioning) and deployed it as a webapp and a FastAPI endpoint using ServiceFoundry 🚀
+
+## Instructions to deploy on ServiceFoundry
+
+<details>
+
+<summary><b><font size="5">Setting up servicefoundry</font></b></summary>
+
+Install and setup servicefoundry on your computer.
+
+```commandline
+pip install servicefoundry
+
+servicefoundry use server https://app.truefoundry.com
+
+servicefoundry login
+```
+
+</details>
+
+
+<details>
+
+<summary><b><font  size="5">Deploying realtime inference</font></b></summary>
+
+
+1. Change working directory to predict folder
+
+```commandline
+
+cd predict
+
+```
+
+2. Create [workspace](https://docs.truefoundry.com/documentation/deploy/concepts/workspace) and [API key](https://docs.truefoundry.com/documentation/deploy/concepts/secrets) on the TrueFoundry platform
+3. Replace the ``MLF_API_KEY`` value `predict.yaml` file with the API Key found in [secrets tab](https://app.truefoundry.com/secrets) of your TrueFoundry account [(Instructions here)](https://docs.truefoundry.com/documentation/deploy/concepts/secrets#how-to-store-secrets-in-truefoundry)
+4. Copy the workspace_fqn of the workspace that you want to use from the [workspace tab](https://app.truefoundry.com/workspaces) of TrueFoundry[(Instructions here)](https://docs.truefoundry.com/documentation/deploy/concepts/workspace#copy-workspace-fqn-fully-qualified-name) and add it in `predict.yaml` file
+
+
+6. To deploy using python script:
+
+```commandline
+
+python predict_deploy.py
+
+```
+
+To deploy using CLI:
+
+```commandline
+
+servicefoundry deploy --file predict/predict_deploy.yaml
+
+```
+
+7. Click on the dashboard link in the terminal to open the service deployment page with FastAPI EndPoint
+
+</details>
+
+<details>
+
+<summary><b><font  size="5">Querying the deployed model</font></b></summary>
+
+This can done via the [fastapi endpoint](https://image-caption-predict-demo-projects.tfy-ctl-euwe1-production.production.truefoundry.com/docs#/default/predict_predict_post) directly via browser.
+
+
+</details>
+
+<details>
+
+<summary><b><font  size="5">Deploying Demo </font></b></summary>
+
+Note: It is necessary to deploy live inference model before being able to deploy a demo
+
+1. Create [workspace](https://docs.truefoundry.com/documentation/deploy/concepts/workspace) and [API key](https://docs.truefoundry.com/documentation/deploy/concepts/secrets) on the TrueFoundry platform
+2. Replace the ``MLF_API_KEY`` value `demo.yaml` file with the API Key found in [secrets tab](https://app.truefoundry.com/secrets) of your TrueFoundry account [(Instructions here)](https://docs.truefoundry.com/documentation/deploy/concepts/secrets#how-to-store-secrets-in-truefoundry)
+3. Copy the workspace_fqn of the workspace that you want to use from the [workspace tab](https://app.truefoundry.com/workspaces) of TrueFoundry and add it in the `demo.yaml` file [(Instructions here)](https://docs.truefoundry.com/documentation/deploy/concepts/workspace#copy-workspace-fqn-fully-qualified-name)
+
+4. To deploy using python script:
+
+```commandline
+
+python demo/demo_deploy.py
+
+```
+
+To deploy using CLI:
+
+```commandline
+
+servicefoundry deploy --file demo/demo_deploy.yaml
+
+```
+
+5. Click on the dashboard link in the terminal
+6. Click on the <b>"Endpoint"</b> link on the dashboard to open the streamlit demo
+
+</details>
--- demo/demo.py
+++ demo/demo.py
@@ -7,6 +7,8 @@ import torch
 from PIL import Image
 from tqdm import tqdm
 import urllib.request
+from itertools import cycle
+import os
 
 model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
@@ -29,45 +31,75 @@ with open("demo/demo.yaml", "r") as stream:
     except yaml.YAMLError as exc:
         print(exc)
 
+# @st.cache(suppress_st_warning=True)
+def show_sample_images():
+    filteredImages = {'First':'sample_images/dog_sitting_in_tub.jpg','Second':'sample_images/man_holding_a_flag.jpg','Third':'sample_images/basketball_player.jpg'} 
+    
+    # with st.form("sample"):
+    cols = cycle(st.columns(3)) 
+    for filteredImage in filteredImages.values():
+        next(cols).image(filteredImage, width=200,)
+    for i, filteredImage in enumerate(filteredImages.values()):
+        if next(cols).button("Predict Caption",key=i):
+            predicted_captions = predict_step([filteredImage],False)
+            st.write(str(i + 1) +'. '+ predicted_captions[0])
+        # if st.button('Predict Caption',key=idx):
+        #     predict_step(filteredImage,False)
+        # st.write('Select images to test the caption algorithm')
+        # checks = st.columns(3)
+        # with checks[0]:
+        #     first = st.checkbox('First')
+        # with checks[1]:
+        #     second = st.checkbox('Second')
+        # with checks[2]:
+        #     third = st.checkbox('Third')
+        # submitted = st.form_submit_button("Submit")
+        # if submitted:
+        #     images = []
+        #     if first:
+        #         images.append(filteredImages['First'])
+        #     if second:
+        #         images.append(filteredImages['Second'])
+        #     if third:
+        #         images.append(filteredImages['Third'])
+            # predicted_captions = predict_step(images,False)
+            # for i,caption in enumerate(predicted_captions):
+            #     st.write(str(i+1)+'. '+caption)
+    # selected_image = st.radio('Which Image you want to select?',('First', 'Second', 'Third'),horizontal=True)
 
+# @st.cache(suppress_st_warning=True)
+def image_uploader():
+    with st.form("uploader"):
+        images = st.file_uploader("Upload Images",accept_multiple_files=True,type=["png","jpg","jpeg"])
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            predicted_captions = predict_step(images,False)
+            for i,caption in enumerate(predicted_captions):
+                st.write(str(i+1)+'. '+caption)
+
+def images_url():
+    with st.form("url"):
+        urls = st.text_input('Enter URL of Images followed by comma for multiple URLs')
+        images = urls.split(',')
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            predicted_captions = predict_step(images,True)
+            for i,caption in enumerate(predicted_captions):
+                st.write(str(i+1)+'. '+caption)
 
 def main():
-    # request_url = env_vars['components'][0]['env']['INFER_URL']
     st.set_page_config(page_title="Image Captioning", page_icon="🖼️")
-    # with open('demo/taxi.jpg','r') as image:
-    # st.image('image/taxi.jpg')
     st.title("Image Caption Prediction")
     st.header('Welcome to Image Caption Prediction!')
     st.write('This is a sample app that demonstrates the prowess of ServiceFoundry ML model deployment.🚀')
     st.write('Visit the [Github](https://github.com/vishank97/image-captioning) repo for detailed exaplaination and to get started right away')
-    with st.form("my_form"):
-        
-        # is_url = st.radio("Image Type:", ("URL", "From File"))
-        # if is_url == "From File":
-        images = st.file_uploader("Upload Images",accept_multiple_files=True,type=["png","jpg","jpeg"])
-        # else:
-        #     images = st.text_input('Image URL')
-
-        features = {
-                "images": images,
-                # "is_url": True if is_url == 'URL' else False
-                "is_url": False
-            }
-            
-        
-        
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            # if is_url == 'From File':
-            #     data = requests.post(url=concat(request_url, "/predict"), json=features).json()
-            # else:
-            # data = requests.post(url=concat(request_url, "/predict"), json=features).json()
-            # if data:
-            #     print(data)
-            #     st.metric(label="Predicted Caption",value=data)
-            # else:
-            #     st.error("Error")
-            st.write(predict_step(images,False))
+    tab1, tab2, tab3 = st.tabs(["Sample Images", "Image from computer", "Image from URL"])
+    with tab1:
+        show_sample_images()
+    with tab2:
+        image_uploader()
+    with tab3:
+        images_url()
 
 def predict_step(images_list,is_url):
     images = []
@@ -75,6 +107,7 @@ def predict_step(images_list,is_url):
         if is_url:
             urllib.request.urlretrieve(image, "file.jpg")
             i_image = Image.open("file.jpg")
+            
         else:
             i_image = Image.open(image)
 
@@ -90,6 +123,8 @@ def predict_step(images_list,is_url):
 
     preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
     preds = [pred.strip() for pred in preds]
+    if is_url:
+        os.remove('file.jpg')
     return preds
 
 if __name__ == '__main__':
